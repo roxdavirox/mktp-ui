74c0b278aca6d2d548ffd86b58c961b396a14e5c
roxdavirox <roxdavirox@gmail.com>
2019-03-22 10:44:51 -0300

    rtl removido do tema

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { createBrowserHistory } from "history";
 import { Router, Route, Switch, Redirect } from "react-router-dom";
 
 import AuthLayout from "layouts/Auth.jsx";
-import RtlLayout from "layouts/RTL.jsx";
 import AdminLayout from "layouts/Admin.jsx";
 
 import "assets/scss/material-dashboard-pro-react.scss?v=1.5.0";
@@ -14,7 +13,6 @@ const hist = createBrowserHistory();
 ReactDOM.render(
   <Router history={hist}>
     <Switch>
-      <Route path="/rtl" component={RtlLayout} />
       <Route path="/auth" component={AuthLayout} />
       <Route path="/admin" component={AdminLayout} />
       <Redirect from="/" to="/admin/dashboard" />
