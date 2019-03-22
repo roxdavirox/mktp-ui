aea09d3f44909dc3b5afe278620778b33ffc6442
roxdavirox <roxdavirox@gmail.com>
2019-03-22 11:14:04 -0300

    aba de opções adicionada

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import Buttons from "views/Components/Buttons.jsx";
 import Calendar from "views/Calendar/Calendar.jsx";
 import Charts from "views/Charts/Charts.jsx";
 import Dashboard from "views/Dashboard/Dashboard.jsx";
+import Option from "./views/Configuration/Options/Option.jsx";
 import ErrorPage from "views/Pages/ErrorPage.jsx";
 import ExtendedForms from "views/Forms/ExtendedForms.jsx";
 import ExtendedTables from "views/Tables/ExtendedTables.jsx";
@@ -14,7 +15,6 @@ import LoginPage from "views/Pages/LoginPage.jsx";
 import Notifications from "views/Components/Notifications.jsx";
 import Panels from "views/Components/Panels.jsx";
 import PricingPage from "views/Pages/PricingPage.jsx";
-import RTLSupport from "views/Pages/RTLSupport.jsx";
 import ReactTables from "views/Tables/ReactTables.jsx";
 import RegisterPage from "views/Pages/RegisterPage.jsx";
 import RegularForms from "views/Forms/RegularForms.jsx";
@@ -48,247 +48,263 @@ var dashRoutes = [
   },
   {
     collapse: true,
-    name: "Pages",
-    icon: Image,
-    state: "pageCollapse",
+    name: "Configurações",
+    icon: DashboardIcon,
+    state: "configCollapse",
     views: [
       {
-        path: "/pricing-page",
-        name: "Pricing Page",
-        mini: "PP",
-        component: PricingPage,
-        layout: "/auth"
-      },
-      {
-        path: "/timeline-page",
-        name: "Timeline Page",
-        mini: "T",
-        component: TimelinePage,
-        layout: "/admin"
-      },
-      {
-        path: "/login-page",
-        name: "Login Page",
-        mini: "L",
-        component: LoginPage,
-        layout: "/auth"
-      },
-      {
-        path: "/register-page",
-        name: "Register Page",
-        mini: "R",
-        component: RegisterPage,
-        layout: "/auth"
-      },
-      {
-        path: "/lock-screen-page",
-        name: "Lock Screen Page",
-        mini: "LS",
-        component: LockScreenPage,
-        layout: "/auth"
-      },
-      {
-        path: "/user-page",
-        name: "User Profile",
-        mini: "UP",
-        component: UserProfile,
-        layout: "/admin"
-      },
-      {
-        path: "/error-page",
-        name: "Error Page",
-        mini: "E",
-        component: ErrorPage,
-        layout: "/auth"
-      }
-    ]
-  },
-  {
-    collapse: true,
-    name: "Components",
-    icon: Apps,
-    state: "componentsCollapse",
-    views: [
-      {
-        collapse: true,
-        name: "Multi Level Collapse",
-        mini: "MC",
-        state: "multiCollapse",
-        views: [
-          {
-            path: "/buttons",
-            name: "Buttons",
-            mini: "B",
-            component: Buttons,
-            layout: "/admin"
-          }
-        ]
-      },
-      {
-        path: "/buttons",
-        name: "Buttons",
-        mini: "B",
-        component: Buttons,
-        layout: "/admin"
-      },
-      {
-        path: "/grid-system",
-        name: "Grid System",
-        mini: "GS",
-        component: GridSystem,
-        layout: "/admin"
-      },
-      {
-        path: "/panels",
-        name: "Panels",
-        mini: "P",
-        component: Panels,
-        layout: "/admin"
-      },
-      {
-        path: "/sweet-alert",
-        name: "Sweet Alert",
-        mini: "SA",
-        component: SweetAlert,
-        layout: "/admin"
-      },
-      {
-        path: "/notifications",
-        name: "Notifications",
-        mini: "N",
-        component: Notifications,
-        layout: "/admin"
-      },
-      {
-        path: "/icons",
-        name: "Icons",
-        mini: "I",
-        component: Icons,
-        layout: "/admin"
-      },
-      {
-        path: "/typography",
-        name: "Typography",
-        mini: "T",
-        component: Typography,
+        path: "/configuration/options",
+        name: "Opções",
+        mini: "O",
+        component: Option,
         layout: "/admin"
       }
     ]
-  },
-  {
-    collapse: true,
-    name: "Forms",
-    icon: "content_paste",
-    state: "formsCollapse",
-    views: [
-      {
-        path: "/regular-forms",
-        name: "Regular Forms",
-        mini: "RF",
-        component: RegularForms,
-        layout: "/admin"
-      },
-      {
-        path: "/extended-forms",
-        name: "Extended Forms",
-        mini: "EF",
-        component: ExtendedForms,
-        layout: "/admin"
-      },
-      {
-        path: "/validation-forms",
-        name: "Validation Forms",
-        mini: "VF",
-        component: ValidationForms,
-        layout: "/admin"
-      },
-      {
-        path: "/wizard",
-        name: "Wizard",
-        mini: "W",
-        component: Wizard,
-        layout: "/admin"
-      }
-    ]
-  },
-  {
-    collapse: true,
-    name: "Tables",
-    icon: GridOn,
-    state: "tablesCollapse",
-    views: [
-      {
-        path: "/regular-tables",
-        name: "Regular Tables",
-        mini: "RT",
-        component: RegularTables,
-        layout: "/admin"
-      },
-      {
-        path: "/extended-tables",
-        name: "Extended Tables",
-        mini: "ET",
-        component: ExtendedTables,
-        layout: "/admin"
-      },
-      {
-        path: "/react-tables",
-        name: "React Tables",
-        mini: "RT",
-        component: ReactTables,
-        layout: "/admin"
-      }
-    ]
-  },
-  {
-    collapse: true,
-    name: "Maps",
-    icon: Place,
-    state: "mapsCollapse",
-    views: [
-      {
-        path: "/google-maps",
-        name: "Google Maps",
-        mini: "GM",
-        component: GoogleMaps,
-        layout: "/admin"
-      },
-      {
-        path: "/full-screen-maps",
-        name: "Full Screen Map",
-        mini: "FSM",
-        component: FullScreenMap,
-        layout: "/admin"
-      },
-      {
-        path: "/vector-maps",
-        name: "Vector Map",
-        mini: "VM",
-        component: VectorMap,
-        layout: "/admin"
-      }
-    ]
-  },
-  {
-    path: "/widgets",
-    name: "Widgets",
-    icon: WidgetsIcon,
-    component: Widgets,
-    layout: "/admin"
-  },
-  {
-    path: "/charts",
-    name: "Charts",
-    icon: Timeline,
-    component: Charts,
-    layout: "/admin"
-  },
-  {
-    path: "/calendar",
-    name: "Calendar",
-    icon: DateRange,
-    component: Calendar,
-    layout: "/admin"
   }
+  // ,
+  // {
+  //   collapse: true,
+  //   name: "Pages",
+  //   icon: Image,
+  //   state: "pageCollapse",
+  //   views: [
+  //     {
+  //       path: "/pricing-page",
+  //       name: "Pricing Page",
+  //       mini: "PP",
+  //       component: PricingPage,
+  //       layout: "/auth"
+  //     },
+  //     {
+  //       path: "/timeline-page",
+  //       name: "Timeline Page",
+  //       mini: "T",
+  //       component: TimelinePage,
+  //       layout: "/admin"
+  //     },
+  //     {
+  //       path: "/login-page",
+  //       name: "Login Page",
+  //       mini: "L",
+  //       component: LoginPage,
+  //       layout: "/auth"
+  //     },
+  //     {
+  //       path: "/register-page",
+  //       name: "Register Page",
+  //       mini: "R",
+  //       component: RegisterPage,
+  //       layout: "/auth"
+  //     },
+  //     {
+  //       path: "/lock-screen-page",
+  //       name: "Lock Screen Page",
+  //       mini: "LS",
+  //       component: LockScreenPage,
+  //       layout: "/auth"
+  //     },
+  //     {
+  //       path: "/user-page",
+  //       name: "User Profile",
+  //       mini: "UP",
+  //       component: UserProfile,
+  //       layout: "/admin"
+  //     },
+  //     {
+  //       path: "/error-page",
+  //       name: "Error Page",
+  //       mini: "E",
+  //       component: ErrorPage,
+  //       layout: "/auth"
+  //     }
+  //   ]
+  // },
+  // {
+  //   collapse: true,
+  //   name: "Components",
+  //   icon: Apps,
+  //   state: "componentsCollapse",
+  //   views: [
+  //     {
+  //       collapse: true,
+  //       name: "Multi Level Collapse",
+  //       mini: "MC",
+  //       state: "multiCollapse",
+  //       views: [
+  //         {
+  //           path: "/buttons",
+  //           name: "Buttons",
+  //           mini: "B",
+  //           component: Buttons,
+  //           layout: "/admin"
+  //         }
+  //       ]
+  //     },
+  //     {
+  //       path: "/buttons",
+  //       name: "Buttons",
+  //       mini: "B",
+  //       component: Buttons,
+  //       layout: "/admin"
+  //     },
+  //     {
+  //       path: "/grid-system",
+  //       name: "Grid System",
+  //       mini: "GS",
+  //       component: GridSystem,
+  //       layout: "/admin"
+  //     },
+  //     {
+  //       path: "/panels",
+  //       name: "Panels",
+  //       mini: "P",
+  //       component: Panels,
+  //       layout: "/admin"
+  //     },
+  //     {
+  //       path: "/sweet-alert",
+  //       name: "Sweet Alert",
+  //       mini: "SA",
+  //       component: SweetAlert,
+  //       layout: "/admin"
+  //     },
+  //     {
+  //       path: "/notifications",
+  //       name: "Notifications",
+  //       mini: "N",
+  //       component: Notifications,
+  //       layout: "/admin"
+  //     },
+  //     {
+  //       path: "/icons",
+  //       name: "Icons",
+  //       mini: "I",
+  //       component: Icons,
+  //       layout: "/admin"
+  //     },
+  //     {
+  //       path: "/typography",
+  //       name: "Typography",
+  //       mini: "T",
+  //       component: Typography,
+  //       layout: "/admin"
+  //     }
+  //   ]
+  // },
+  // {
+  //   collapse: true,
+  //   name: "Forms",
+  //   icon: "content_paste",
+  //   state: "formsCollapse",
+  //   views: [
+  //     {
+  //       path: "/regular-forms",
+  //       name: "Regular Forms",
+  //       mini: "RF",
+  //       component: RegularForms,
+  //       layout: "/admin"
+  //     },
+  //     {
+  //       path: "/extended-forms",
+  //       name: "Extended Forms",
+  //       mini: "EF",
+  //       component: ExtendedForms,
+  //       layout: "/admin"
+  //     },
+  //     {
+  //       path: "/validation-forms",
+  //       name: "Validation Forms",
+  //       mini: "VF",
+  //       component: ValidationForms,
+  //       layout: "/admin"
+  //     },
+  //     {
+  //       path: "/wizard",
+  //       name: "Wizard",
+  //       mini: "W",
+  //       component: Wizard,
+  //       layout: "/admin"
+  //     }
+  //   ]
+  // },
+  // {
+  //   collapse: true,
+  //   name: "Tables",
+  //   icon: GridOn,
+  //   state: "tablesCollapse",
+  //   views: [
+  //     {
+  //       path: "/regular-tables",
+  //       name: "Regular Tables",
+  //       mini: "RT",
+  //       component: RegularTables,
+  //       layout: "/admin"
+  //     },
+  //     {
+  //       path: "/extended-tables",
+  //       name: "Extended Tables",
+  //       mini: "ET",
+  //       component: ExtendedTables,
+  //       layout: "/admin"
+  //     },
+  //     {
+  //       path: "/react-tables",
+  //       name: "React Tables",
+  //       mini: "RT",
+  //       component: ReactTables,
+  //       layout: "/admin"
+  //     }
+  //   ]
+  // },
+  // {
+  //   collapse: true,
+  //   name: "Maps",
+  //   icon: Place,
+  //   state: "mapsCollapse",
+  //   views: [
+  //     {
+  //       path: "/google-maps",
+  //       name: "Google Maps",
+  //       mini: "GM",
+  //       component: GoogleMaps,
+  //       layout: "/admin"
+  //     },
+  //     {
+  //       path: "/full-screen-maps",
+  //       name: "Full Screen Map",
+  //       mini: "FSM",
+  //       component: FullScreenMap,
+  //       layout: "/admin"
+  //     },
+  //     {
+  //       path: "/vector-maps",
+  //       name: "Vector Map",
+  //       mini: "VM",
+  //       component: VectorMap,
+  //       layout: "/admin"
+  //     }
+  //   ]
+  // },
+  // {
+  //   path: "/widgets",
+  //   name: "Widgets",
+  //   icon: WidgetsIcon,
+  //   component: Widgets,
+  //   layout: "/admin"
+  // },
+  // {
+  //   path: "/charts",
+  //   name: "Charts",
+  //   icon: Timeline,
+  //   component: Charts,
+  //   layout: "/admin"
+  // },
+  // {
+  //   path: "/calendar",
+  //   name: "Calendar",
+  //   icon: DateRange,
+  //   component: Calendar,
+  //   layout: "/admin"
+  // }
 ];
 export default dashRoutes;
