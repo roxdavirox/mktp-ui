2eb474bf75096a20c299a5cfcb9c1fe33bdb5787
roxdavirox <roxdavirox@gmail.com>
2019-03-22 13:36:43 -0300

    Componente MuiDatatables adicionado, customizado e renderizado na view com dados estaticos

---
+++ src/components/Table/MuiDatatables.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import MUIDataTable from "mui-datatables";
+import PropTypes from "prop-types";
+
+const CustomMUIDataTable = ({ title, data, columns, options }) => (
+  <MUIDataTable
+    title={title ? title : "Titulo"}
+    data={data}
+    columns={columns}
+    options={options}
+  />
+);
+
+CustomMUIDataTable.propTypes = {
+  title: PropTypes.any.isRequired,
+  data: PropTypes.any.isRequired,
+  columns: PropTypes.any.isRequired,
+  options: PropTypes.any.isRequired
+};
+
+export default CustomMUIDataTable;
--- src/views/Configuration/Options/Option.jsx
+++ src/views/Configuration/Options/Option.jsx
@@ -1,5 +1,60 @@
 import React from "react";
 
-const Option = () => <h1>Ola</h1>;
+import CustomMUIDataTable from "components/Table/MuiDatatables";
+
+const columns = [
+  {
+    name: "name",
+    label: "Name",
+    options: {
+      filter: true,
+      sort: true
+    }
+  },
+  {
+    name: "company",
+    label: "Company",
+    options: {
+      filter: true,
+      sort: false
+    }
+  },
+  {
+    name: "city",
+    label: "City",
+    options: {
+      filter: true,
+      sort: false
+    }
+  },
+  {
+    name: "state",
+    label: "State",
+    options: {
+      filter: true,
+      sort: false
+    }
+  }
+];
+
+const data = [
+  { name: "Joe James", company: "Test Corp", city: "Yonkers", state: "NY" },
+  { name: "John Walsh", company: "Test Corp", city: "Hartford", state: "CT" },
+  { name: "Bob Herm", company: "Test Corp", city: "Tampa", state: "FL" },
+  { name: "James Houston", company: "Test Corp", city: "Dallas", state: "TX" }
+];
+
+const options = {
+  filterType: "checkbox"
+};
+
+const Option = () => (
+  <CustomMUIDataTable
+    title={"Lista de clientes"}
+    data={data}
+    columns={columns}
+    options={options}
+  />
+);
 
 export default Option;
