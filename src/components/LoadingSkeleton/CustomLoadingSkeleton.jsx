414e5c5fe068da20a2c70fd3a296d20dcd36230b
roxdavirox <roxdavirox@gmail.com>
2019-03-28 08:45:14 -0300

    quando todas as opções forem deletadas o skeleton mostra uma mensagem ao inves do loading

--- src/components/LoadingSkeleton/CustomLoadingSkeleton.jsx
+++ src/components/LoadingSkeleton/CustomLoadingSkeleton.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import Skeleton from "react-loading-skeleton";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
-const CustomLoadingSkeleton = () => (
-  <div style={{ fontSize: 25, lineHeight: 2 }}>
-    <Skeleton count={8} height={50} />
-  </div>
-);
+const CustomLoadingSkeleton = ({ isLoading }) =>
+  isLoading ? (
+    <div style={{ fontSize: 25, lineHeight: 2 }}>
+      <Skeleton count={8} height={50} />
+    </div>
+  ) : (
+    "Nenhuma opção"
+  );
 
-export default CustomLoadingSkeleton;
+const mapStateToProps = store => ({ isLoading: store.optionsState.loading });
+
+CustomLoadingSkeleton.propTypes = {
+  isLoading: PropTypes.any.isRequired
+};
+
+export default connect(mapStateToProps)(CustomLoadingSkeleton);
