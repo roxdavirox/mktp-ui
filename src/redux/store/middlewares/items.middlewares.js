4ae6d87b4f8d235c03bda0eceb21bf6662eb9320
roxdavirox <roxdavirox@gmail.com>
2019-04-03 10:47:54 -0300

    middleware para deletar itens

--- src/redux/store/middlewares/items.middlewares.js
+++ src/redux/store/middlewares/items.middlewares.js
@@ -3,7 +3,10 @@ import {
   getItemsSuccess,
   POST_ITEM_BEGIN,
   postItemSuccess,
-  postItemFailure
+  postItemFailure,
+  DELETE_ITEMS_BEGIN,
+  deleteItemsSuccess,
+  deleteItemsFailure
 } from "../../actions/items.actions";
 
 const url = "https://mktp.azurewebsites.net/api";
@@ -44,6 +47,51 @@ export const postItemMiddleware = ({
       .then(res => res.json())
       .then(item => dispatch(postItemSuccess(item)))
       .catch(error => dispatch(postItemFailure(error)));
+  }
+
+  next(action);
+};
+
+export const deleteItemsMiddleware = ({
+  dispatch,
+  getState
+}) => next => action => {
+  if (action.type === DELETE_ITEMS_BEGIN) {
+    const { deletedItemsIds, snack } = action.playload;
+
+    const { items: prevItems } = getState().itemsState;
+
+    const items = prevItems.filter(
+      ({ id }) => deletedItemsIds.indexOf(id) === -1
+    );
+
+    const body = {
+      itemsIds: deletedItemsIds
+    };
+
+    const request = {
+      method: "DELETE",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "applicatino/json"
+      },
+      body: JSON.stringify(body)
+    };
+
+    fetch(`${url}/items`, request)
+      .then(res => res.json())
+      .then(res => {
+        const { deletedItemsCount: count } = res;
+        if (count) {
+          dispatch(deleteItemsSuccess(items));
+          snack(`${count} ite${count == 1 ? "m deletado" : "ns deletados"}`, {
+            variant: "success",
+            autoHideDuration: 2000
+          });
+        }
+        return res;
+      })
+      .catch(error => dispatch(deleteItemsFailure(error)));
   }
 
   next(action);
