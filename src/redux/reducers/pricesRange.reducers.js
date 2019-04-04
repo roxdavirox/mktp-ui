460a9533943c6af4f96c6e1d11979b16653f0e91
roxdavirox <roxdavirox@gmail.com>
2019-04-04 16:25:09 -0300

    reducer de prices range implementado para dar get nos dados iniciais do data table

--- src/redux/reducers/pricesRange.reducers.js
+++ src/redux/reducers/pricesRange.reducers.js
@@ -1,5 +1,8 @@
-
-
+import {
+  FETCH_PRICES_RANGE_BEGIN,
+  FETCH_PRICES_RANGE_SUCCESS,
+  FETCH_PRICES_RANGE_FAILURE
+} from "../actions/pricesRange.actions";
 
 const initialState = {
   pricesRange: [],
@@ -7,3 +10,41 @@ const initialState = {
   error: null,
   openAlert: false
 };
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case FETCH_PRICES_RANGE_BEGIN: {
+      return {
+        ...state,
+        pricesRange: [],
+        loading: true,
+        error: null
+      };
+    }
+
+    case FETCH_PRICES_RANGE_SUCCESS: {
+      const { pricesRange } = action.playload;
+
+      return {
+        ...state,
+        pricesRange,
+        loading: false
+      };
+    }
+
+    case FETCH_PRICES_RANGE_FAILURE: {
+      const { error } = action.playload;
+
+      return {
+        ...state,
+        error,
+        loading: false
+      };
+    }
+
+    default:
+      return {
+        ...state
+      };
+  }
+}
