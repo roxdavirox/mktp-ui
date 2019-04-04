bbf00b77a22114e431323bd383c1bc7ff35c2b1b
roxdavirox <roxdavirox@gmail.com>
2019-04-04 17:05:10 -0300

    renderizando prices table na view

---
+++ src/views/Configuration/Price/PriceTable.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import PriceTableMuiDatatable from "components/Table/PriceTableMuiDatatable";
+
+const PriceTable = () => <PriceTableMuiDatatable />;
+
+export default PriceTable;
