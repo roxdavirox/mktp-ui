2df182d6fd3b27ef60bd4dc061f052a562a10fcc
roxdavirox <roxdavirox@gmail.com>
2019-04-04 16:56:22 -0300

    price range data table com redux e fetch

--- src/components/Table/PriceTableMuiDatatable.jsx
+++ src/components/Table/PriceTableMuiDatatable.jsx
@@ -2,11 +2,64 @@ import React from "react";
 import CustomMUIDataTable from "./MuiDatatables";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
+import CustomLoadingSkeleton from "components/LoadingSkeleton/CustomLoadingSkeleton.jsx";
+
+import { fetchPricesRangeBegin } from "../../redux/actions/pricesRange.actions";
 
 class PriceTableMuiDatatable extends React.Component {
+  state = {
+    columns: [
+      {
+        name: "name",
+        label: "Nome",
+        options: {
+          filter: true,
+          sort: true
+        }
+      },
+      {
+        name: "idPriceRange",
+        options: {
+          display: "excluded",
+          sort: false,
+          filter: false
+        }
+      }
+    ],
+    options: {
+      filterType: "checkbox",
+      textLabels: {
+        body: {
+          noMatch: <CustomLoadingSkeleton />
+        }
+      }
+    }
+  };
+
+  componentDidMount = ({ fetchPricesRangeBegin }) => {
+    fetchPricesRangeBegin();
+  };
+
   render = () => {
-    return <CustomMUIDataTable />;
+    const { data } = this.props;
+    const { columns, options } = this.state;
+
+    return (
+      <CustomMUIDataTable data={data} columns={columns} options={options} />
+    );
   };
 }
 
-export default PriceTableMuiDatatable;
+PriceTableMuiDatatable.propTypes = {
+  data: PropTypes.any.isRequired,
+  fetchPricesRangeBegin: PropTypes.any.isRequired
+};
+
+const mapStateToProps = store => ({
+  data: store.pricesRangeState.pricesRange
+});
+
+export default connect(
+  mapStateToProps,
+  { fetchPricesRangeBegin }
+)(PriceTableMuiDatatable);
