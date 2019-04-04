de55ab063794d826fddb2f6b367fdfbc5cfbf2d8
roxdavirox <roxdavirox@gmail.com>
2019-04-04 16:37:22 -0300

    fetch prices range middlware implementado

---
+++ src/redux/store/middlewares/pricesRange.middlewares.js
@@ -0,0 +1,18 @@
+import {
+  FETCH_PRICES_RANGE_BEGIN,
+  fetchPricesRangeSuccess,
+  fetchPricesRangeFailure
+} from "../../actions/pricesRange.actions";
+
+const url = "https://mktp.azurewebsites.net/api/PriceRange";
+
+export const fetchPricesRangeBegin = ({ dispatch }) => next => action => {
+  if (action.type === FETCH_PRICES_RANGE_BEGIN) {
+    fetch(url)
+      .then(res => res.json())
+      .then(({ pricesRange }) => dispatch(fetchPricesRangeSuccess(pricesRange)))
+      .then(error => dispatch(fetchPricesRangeFailure(error)));
+  }
+
+  next(action);
+};
