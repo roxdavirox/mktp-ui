528d0a7706f944f45b7a8a9904c18251cd27bafb
roxdavirox <roxdavirox@gmail.com>
2019-04-05 10:09:05 -0300

    Middleware para post de price table implementado

--- src/redux/store/middlewares/pricesRange.middlewares.js
+++ src/redux/store/middlewares/pricesRange.middlewares.js
@@ -1,7 +1,10 @@
 import {
   FETCH_PRICES_RANGE_BEGIN,
   fetchPricesRangeSuccess,
-  fetchPricesRangeFailure
+  fetchPricesRangeFailure,
+  POST_PRICE_RANGE_BEGIN,
+  postPriceRangeSuccess,
+  postPriceRangeFailure
 } from "../../actions/pricesRange.actions";
 
 const url = "https://mktp.azurewebsites.net/api/PriceRange";
@@ -12,6 +15,37 @@ export const fetchPricesRangeMiddleware = ({ dispatch }) => next => action => {
       .then(res => res.json())
       .then(({ pricesRange }) => dispatch(fetchPricesRangeSuccess(pricesRange)))
       .then(error => dispatch(fetchPricesRangeFailure(error)));
+  }
+
+  next(action);
+};
+
+export const postPriceRangeMiddleware = ({ dispatch }) => next => action => {
+  if (action.type === POST_PRICE_RANGE_BEGIN) {
+    const { priceRange, snack } = action.playload;
+
+    const request = {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(priceRange)
+    };
+
+    fetch(url, request)
+      .then(res => res.json())
+      .then(priceRange => {
+        snack(`Tabela de preço ${priceRange.name} adicionada com sucesso!`, {
+          variant: "success",
+          autoHideDuration: 2000
+        });
+        dispatch(postPriceRangeSuccess(priceRange));
+      })
+      .catch(error => {
+        snack(`Error: ${error}`);
+        dispatch(postPriceRangeFailure(error));
+      });
   }
 
   next(action);
