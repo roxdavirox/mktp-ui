0bf5816752571f0f6ca0578d373f5772044683a7
roxdavirox <roxdavirox@gmail.com>
2019-04-05 09:48:47 -0300

    snack provider removido da view de options

--- src/views/Configuration/Options/Option.jsx
+++ src/views/Configuration/Options/Option.jsx
@@ -1,19 +1,8 @@
 /* eslint-disable react/display-name */
 import React from "react";
-import { SnackbarProvider } from "notistack";
 
 import OptionMuiDatatable from "components/Table/OptionMuiDatatable";
 
-const Option = () => (
-  <SnackbarProvider
-    maxSnack={2}
-    anchorOrigin={{
-      vertical: "top",
-      horizontal: "right"
-    }}
-  >
-    <OptionMuiDatatable />
-  </SnackbarProvider>
-);
+const Option = () => <OptionMuiDatatable />;
 
 export default Option;
