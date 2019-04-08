1408803b2ae7e0aceee19a4e984134800fdcd8e7
roxdavirox <roxdavirox@gmail.com>
2019-04-08 16:23:25 -0300

    price adicionado ao reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import options from "./options.reducers";
 import items from "./items.reducers";
 import pricesRange from "./pricesRange.reducers";
+import price from "./prices.reducers";
 import { combineReducers } from "redux";
 
 export const reducers = combineReducers({
   optionsState: options,
   itemsState: items,
-  pricesRangeState: pricesRange
+  pricesRangeState: pricesRange,
+  priceState: price
 });
