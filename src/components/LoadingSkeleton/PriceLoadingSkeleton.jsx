2b77bb8403b4447b83b0d4c75bb54415bd3e61a0
roxdavirox <roxdavirox@gmail.com>
2019-04-08 19:18:25 -0300

    loading para prices

---
+++ src/components/LoadingSkeleton/PriceLoadingSkeleton.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Skeleton from "react-loading-skeleton";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+
+const PriceLoadingSkeleton = ({ isLoading }) =>
+  isLoading ? (
+    <div style={{ fontSize: 25, lineHeight: 2 }}>
+      <Skeleton count={8} height={50} />
+    </div>
+  ) : (
+    "Nenhuma preço cadastrado"
+  );
+
+const mapStateToProps = store => ({
+  isLoading: store.pricesState.loading
+});
+
+PriceLoadingSkeleton.propTypes = {
+  isLoading: PropTypes.any.isRequired
+};
+
+export default connect(mapStateToProps)(PriceLoadingSkeleton);
