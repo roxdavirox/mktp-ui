cd3eb464bdb0005cd1613864de3b188ba1c20d74
roxdavirox <roxdavirox@gmail.com>
2019-04-08 19:19:30 -0300

    fetch actions para carregar prices

--- src/redux/actions/prices.actions.js
+++ src/redux/actions/prices.actions.js
@@ -4,3 +4,23 @@ export const setPriceRangeId = idPriceRange => ({
   type: SET_PRICE_RANGE_ID,
   playload: { idPriceRange }
 });
+
+export const FETCH_PRICES_BEGIN = "FETCH_PRICES_BEGIN";
+
+export const fetchPricesBegin = () => ({
+  type: FETCH_PRICES_BEGIN
+});
+
+export const FETCH_PRICES_SUCCESS = "FETCH_PRICES_SUCCESS";
+
+export const fetchPricesSuccess = prices => ({
+  type: FETCH_PRICES_SUCCESS,
+  playload: { prices }
+});
+
+export const FETCH_PRICES_FAILURE = "FETCH_PRICES_FAILURE";
+
+export const fetchPricesFailure = error => ({
+  type: FETCH_PRICES_FAILURE,
+  playload: { error }
+});
