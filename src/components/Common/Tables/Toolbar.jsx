a25df84348aded8ae79facae805306004a6b4462
roxdavirox <roxdavirox@gmail.com>
2019-04-28 19:58:48 -0300

    toolbar refatorado

--- src/components/Common/Tables/Toolbar.jsx
+++ src/components/Common/Tables/Toolbar.jsx
@@ -3,30 +3,27 @@ import IconButton from "@material-ui/core/IconButton";
 import Tooltip from "@material-ui/core/Tooltip";
 import AddIcon from "@material-ui/icons/Add";
 import { withStyles } from "@material-ui/core/styles";
+import PropTypes from "prop-types";
 
 const defaultToolbarStyles = {
   iconButton: {}
 };
 
-class CustomToolbar extends React.Component {
-  render() {
-    const { classes, title, onClick, ...rest } = this.props;
+const CustomToolbar = ({ classes, title, ...rest }) => (
+  <>
+    <Tooltip title={title ? title : "custom Add"}>
+      <IconButton className={classes.iconButton} {...rest}>
+        <AddIcon />
+      </IconButton>
+    </Tooltip>
+  </>
+);
 
-    return (
-      <React.Fragment>
-        <Tooltip title={title ? title : "custom Add"}>
-          <IconButton
-            className={classes.iconButton}
-            onClick={onClick}
-            {...rest}
-          >
-            <AddIcon className={classes.deleteIcon} />
-          </IconButton>
-        </Tooltip>
-      </React.Fragment>
-    );
-  }
-}
+CustomToolbar.propTypes = {
+  classes: PropTypes.object.isRequired,
+  title: PropTypes.string,
+  onClick: PropTypes.func
+};
 
 export default withStyles(defaultToolbarStyles, { name: "CustomToolbar" })(
   CustomToolbar
