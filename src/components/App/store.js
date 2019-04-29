38e9cad6ff7b88bc0c474a759f52d632f33f0ca2
roxdavirox <roxdavirox@gmail.com>
2019-04-29 11:54:14 -0300

    referencias do redux atualizadas

--- src/components/App/store.js
+++ src/components/App/store.js
@@ -1,11 +1,11 @@
 import { createStore, applyMiddleware, compose } from "redux";
-import { reducers } from "../reducers";
+import { reducers } from "./reducers";
 
 import {
   postOptionMiddleware,
   deleteOptionsMiddleware,
   fetchOptionsMiddleware
-} from "../middlewares/options";
+} from "../Option/optionMiddlewares";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
