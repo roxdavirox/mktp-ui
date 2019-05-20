dc58c5aada46d9f027fa0fd6bc7a74191e52f991
roxdavirox <roxdavirox@gmail.com>
2019-05-20 19:25:32 -0300

    redirect page que renderiza os itens das opções

--- src/base/components/item/ItemPageRedirect.jsx
+++ src/base/components/item/ItemPageRedirect.jsx
@@ -1,12 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { deleteItems, fetchItems } from './actions';
 import ItemDatatable from './Datatable';
 
 import { getOptionsItems } from './selectors';
+
+const PageRedirect = ({ data }) => <ItemDatatable data={data} />;
 
 const mapStateToProps = store => ({
   data: getOptionsItems(store)
 });
 
+PageRedirect.propTypes = {
+  data: PropTypes.any.isRequired
+};
 
+export default connect(mapStateToProps)(PageRedirect);
