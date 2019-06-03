03345f771755ffed2deb80516fffadded5c1cf2c
roxdavirox <roxdavirox@gmail.com>
2019-06-03 19:27:21 -0300

    selector dos prices

--- src/base/components/priceTable/price/selectors.js
+++ src/base/components/priceTable/price/selectors.js
@@ -1,1 +1,12 @@
-export const getPrices = store => [];
+export const getPricesState = store => store.prices;
+
+export const getPriceList = store =>
+  getPricesState(store) ? getPricesState(store).allIds : [];
+
+export const getPriceById = (id, store) =>
+  getPricesState(store) ? getPricesState(store).byId[id] : {};
+
+export const getPrices = store =>
+  getPricesState(store)
+    ? getPriceList(store).map(id => getPriceById(id, store))
+    : [];
