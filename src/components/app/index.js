0cdeb8c87ce5ad8cb4842c01952de207f3f1dd86
roxdavirox <roxdavirox@gmail.com>
2019-07-19 13:54:41 -0300

    base removed

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import AppProviders from 'base/providers';
+import AppProviders from 'providers';
 import 'assets/scss/material-dashboard-pro-react.scss?v=1.5.0';
 
 const App = () => <AppProviders />;
