475e54127d47dc93f312f9605063e87ce81de6bc
roxdavirox <roxdavirox@gmail.com>
2019-07-19 13:59:14 -0300

    update refs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import 'assets/scss/material-dashboard-pro-react.scss?v=1.5.0';
-import App from 'base/components/app';
+import App from 'components/app';
 
 const rootElement = document.getElementById('root');
 
