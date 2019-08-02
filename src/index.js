5c15d4bffcb15235539832a6a8895eddd0267269
roxdavirox <roxdavirox@gmail.com>
2019-08-02 18:45:13 -0300

    ffix

--- src/index.js
+++ src/index.js
@@ -3,12 +3,6 @@ import { render } from 'react-dom';
 import 'assets/scss/material-dashboard-pro-react.scss?v=1.5.0';
 import App from 'components/app';
 
-import { CookiesProvider } from 'react-cookie';
 const rootElement = document.getElementById('root');
 
-render(
-  <CookiesProvider>
-    <App />
-  </CookiesProvider>,
-  rootElement
-);
+render(<App />, rootElement);
--- src/providers/index.js
+++ src/providers/index.js
@@ -4,14 +4,18 @@ import Store from './store';
 import Notifications from './snack';
 import Theme from './muiTheme';
 import Router from './router';
+import { CookiesProvider } from 'react-cookie';
+
 const Providers = () => (
-  <Store>
-    <Notifications>
-      <Theme>
-        <Router />
-      </Theme>
-    </Notifications>
-  </Store>
+  <CookiesProvider>
+    <Store>
+      <Notifications>
+        <Theme>
+          <Router />
+        </Theme>
+      </Notifications>
+    </Store>
+  </CookiesProvider>
 );
 
 export default Providers;
