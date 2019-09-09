1e715ec4f3d542972334d5a0eec7d797340e3923
roxdavirox <roxdavirox@gmail.com>
2019-09-09 13:10:28 -0300

    snack no template

--- src/components/pages/Template/index.js
+++ src/components/pages/Template/index.js
@@ -2,6 +2,7 @@
 /* eslint-disable no-extra-boolean-cast */
 import React, { useState, useEffect } from 'react';
 import { withStyles } from '@material-ui/core/styles';
+import { withSnackbar } from 'notistack';
 
 import GridContainer from 'components/theme/Grid/GridContainer.jsx';
 import GridItem from 'components/theme/Grid/GridItem.jsx';
@@ -23,118 +24,128 @@ const style = {
   }
 };
 
-const TemplatePage = withStyles(style)(({ classes, location }) => {
-  const [openDialog, setDialogState] = useState(false);
-  const [dialogType, setDialogType] = useState('');
-  const [productTemplates, setProductTemplates] = useState([]);
-  const [templatesCategory, setTemplatesCategory] = useState([]);
-  const [templateCategorySelectedId, setTemplateId] = useState('a');
+const TemplatePage = withStyles(style)(
+  ({ classes, location, enqueueSnackbar: snack }) => {
+    const [openDialog, setDialogState] = useState(false);
+    const [dialogType, setDialogType] = useState('');
+    const [productTemplates, setProductTemplates] = useState([]);
+    const [templatesCategory, setTemplatesCategory] = useState([]);
+    const [templateCategorySelectedId, setTemplateId] = useState('a');
 
-  const {
-    state: { product }
-  } = location;
+    const {
+      state: { product }
+    } = location;
 
-  useEffect(() => {
-    const templateCategoryId = templateCategorySelectedId;
+    useEffect(() => {
+      const templateCategoryId = templateCategorySelectedId;
 
-    const endpoint = getEndpoint(
-      templateCategorySelectedId !== 'a'
-        ? `/product-templates/${templateCategoryId}`
-        : `/product-templates/all/${product._id}`
-    );
+      const endpoint = getEndpoint(
+        templateCategorySelectedId !== 'a'
+          ? `/product-templates/${templateCategoryId}`
+          : `/product-templates/all/${product._id}`
+      );
 
-    fetch(endpoint)
-      .then(res => res.json())
-      .then(({ productTemplates }) => setProductTemplates(productTemplates))
-      .catch(e => console.log(e));
-  }, [templateCategorySelectedId, templatesCategory]);
+      fetch(endpoint)
+        .then(res => res.json())
+        .then(({ productTemplates }) => setProductTemplates(productTemplates))
+        .catch(e => console.log(e));
+    }, [templateCategorySelectedId, templatesCategory]);
 
-  useEffect(() => {
-    const { _id: productId } = product;
-    const endpoint = getEndpoint(`/templates-category/${productId}`);
+    useEffect(() => {
+      const { _id: productId } = product;
+      const endpoint = getEndpoint(`/templates-category/${productId}`);
 
-    fetch(endpoint)
-      .then(res => res.json())
-      .then(({ templatesCategory }) => setTemplatesCategory(templatesCategory))
-      .catch(e => console.log(e));
-  }, []);
+      fetch(endpoint)
+        .then(res => res.json())
+        .then(({ templatesCategory }) =>
+          setTemplatesCategory(templatesCategory)
+        )
+        .catch(e => console.log(e));
+    }, []);
 
-  const handleChangeTemplateSelected = id => setTemplateId(id);
+    const handleChangeTemplateSelected = id => setTemplateId(id);
 
-  const handleAddCategory = async categoryName => {
-    const { _id: productId } = product;
-    const endpoint = getEndpoint(`/templates-category/${productId}`);
-    const request = createPostRequest({ name: categoryName });
-    fetch(endpoint, request)
-      .then(res => res.json())
-      .then(({ templateCategory }) =>
-        setTemplatesCategory([...templatesCategory, templateCategory])
-      )
-      .catch(e => console.log(e));
-  };
-
-  const handleAddDesignTemplate = async (name, img, psd) => {
-    const data = new FormData();
-    data.append('name', name);
-    data.append('productId', product._id);
-    data.append('img', img);
-    data.append('psd', psd);
-    // send request to backend and backend send to CC-API
-    const request = {
-      method: 'POST',
-      body: data
+    const handleAddCategory = async categoryName => {
+      const { _id: productId } = product;
+      const endpoint = getEndpoint(`/templates-category/${productId}`);
+      const request = createPostRequest({ name: categoryName });
+      fetch(endpoint, request)
+        .then(res => res.json())
+        .then(({ templateCategory }) =>
+          setTemplatesCategory([...templatesCategory, templateCategory])
+        )
+        .catch(e => console.log(e));
     };
-    const templateCategoryId = templateCategorySelectedId;
-    console.log(
-      'enviando arquivos para categoria de template com id:',
-      templateCategoryId
-    );
-    const endpoint = getEndpoint(`/product-templates/${templateCategoryId}`);
 
-    fetch(endpoint, request)
-      .then(res => res.json())
-      .then(res => console.log('response do upload de arquivos: ', res));
-  };
+    const handleAddDesignTemplate = async (name, img, psd) => {
+      const data = new FormData();
+      data.append('name', name);
+      data.append('productId', product._id);
+      data.append('img', img);
+      data.append('psd', psd);
+      // send request to backend and backend send to CC-API
+      const request = {
+        method: 'POST',
+        body: data
+      };
+      const templateCategoryId = templateCategorySelectedId;
+      console.log(
+        'enviando arquivos para categoria de template com id:',
+        templateCategoryId
+      );
+      const endpoint = getEndpoint(`/product-templates/${templateCategoryId}`);
 
-  const handleOpenDialog = type => {
-    setDialogState(true);
-    setDialogType(type);
-  };
+      fetch(endpoint, request)
+        .then(res => res.json())
+        .then(({ productTemplate }) => {
+          setProductTemplates([...productTemplates, productTemplate]);
+          snack(`Template adicionado com sucesso!`, {
+            variant: 'success',
+            autoHideDuration: 2000
+          });
+        });
+    };
 
-  return (
-    <>
-      {openDialog && (
-        <Dialog
-          open={openDialog}
-          type={dialogType}
-          onClose={() => setDialogState(false)}
-          // onOpen={handleOpenDialog}
-          onAddDesignTemplate={handleAddDesignTemplate}
-          onAddCategory={handleAddCategory}
-        />
-      )}
-      <GridContainer>
-        <GridItem xs={4} sm={4} md={4} lg={3}>
-          <TemplateCategories
-            categories={templatesCategory}
-            onSelectCategory={handleChangeTemplateSelected}
-            selectedEnabled={templateCategorySelectedId}
-            onOpenAddCategoryDialog={() => handleOpenDialog('category')}
+    const handleOpenDialog = type => {
+      setDialogState(true);
+      setDialogType(type);
+    };
+
+    return (
+      <>
+        {openDialog && (
+          <Dialog
+            open={openDialog}
+            type={dialogType}
+            onClose={() => setDialogState(false)}
+            // onOpen={handleOpenDialog}
+            onAddDesignTemplate={handleAddDesignTemplate}
+            onAddCategory={handleAddCategory}
           />
-        </GridItem>
-        <GridItem xs={8} sm={4} md={4} lg={8}>
-          <GridContainer className={classes.container}>
-            <ProductTemplates
-              productTemplates={productTemplates}
-              location={location}
-              onOpenDialog={() => handleOpenDialog('design')}
-              templateCategorySelectedId={templateCategorySelectedId}
+        )}
+        <GridContainer>
+          <GridItem xs={4} sm={4} md={4} lg={3}>
+            <TemplateCategories
+              categories={templatesCategory}
+              onSelectCategory={handleChangeTemplateSelected}
+              selectedEnabled={templateCategorySelectedId}
+              onOpenAddCategoryDialog={() => handleOpenDialog('category')}
             />
-          </GridContainer>
-        </GridItem>
-      </GridContainer>
-    </>
-  );
-});
+          </GridItem>
+          <GridItem xs={8} sm={4} md={4} lg={8}>
+            <GridContainer className={classes.container}>
+              <ProductTemplates
+                productTemplates={productTemplates}
+                location={location}
+                onOpenDialog={() => handleOpenDialog('design')}
+                templateCategorySelectedId={templateCategorySelectedId}
+              />
+            </GridContainer>
+          </GridItem>
+        </GridContainer>
+      </>
+    );
+  }
+);
 
-export default TemplatePage;
+export default withSnackbar(TemplatePage);
