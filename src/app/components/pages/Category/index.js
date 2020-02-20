fefc667113813c6161c849b37bd9e201ae9a621c
roxdavirox <roxdavirox@gmail.com>
2020-02-20 11:49:18 -0300

    bread categories

--- src/app/components/pages/Category/index.js
+++ src/app/components/pages/Category/index.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Container from '@material-ui/core/Container';
 import { withSnackbar } from 'notistack';
+import { Breadcrumb } from 'matx';
 import Datatable from './Datatable';
 import Dialog from './Dialog';
 import { fetchCategories } from 'app/redux/actions/Category.actions';
@@ -18,7 +19,12 @@ const CategoryPage = props => {
   const [open, setOpen] = useState(false);
   return (
     <>
-      <Container maxWidth="xl">
+      <Container maxWidth="xl" className="m-sm-30">
+        <div className="mb-sm-30">
+          <Breadcrumb
+            routeSegments={[{ name: 'Categorias', path: '/categories' }]}
+          />
+        </div>
         {open && <Dialog open={open} onClose={() => setOpen(false)} />}
         <Datatable data={data} onOpen={() => setOpen(true)} {...props} />
       </Container>
