a231fcb62f1df3f4a8b2f2c2f447a268585ce49f
roxdavirox <roxdavirox@gmail.com>
2020-03-06 17:45:54 -0300

    tooltip working

--- src/app/components/pages/Product/CreateProduct/TableHeadCell.jsx
+++ src/app/components/pages/Product/CreateProduct/TableHeadCell.jsx
@@ -1,7 +1,9 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable react/display-name */
 import React from 'react';
 import TableCell from '@material-ui/core/TableCell';
 import { makeStyles } from '@material-ui/core/styles';
+import Tooltip from '@material-ui/core/Tooltip';
 
 const useStyles = makeStyles({
   TableCell: { fontWeight: 500, padding: '0 0 0 2em', cursor: 'pointer' }
@@ -9,7 +11,13 @@ const useStyles = makeStyles({
 
 const CustomTableHeadCell = props => {
   const classes = useStyles();
-  return <TableCell className={classes.TableCell} {...props} />;
+  return (
+    <TableCell className={classes.TableCell} {...props}>
+      <Tooltip title={props.children} placement="bottom">
+        <span>{props.children}</span>
+      </Tooltip>
+    </TableCell>
+  );
 };
 
 export default CustomTableHeadCell;
