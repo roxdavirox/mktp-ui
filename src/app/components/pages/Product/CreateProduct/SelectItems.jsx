82dd218f231fff26958487617ca479e8b8466f5c
roxdavirox <roxdavirox@gmail.com>
2020-03-06 09:46:49 -0300

    compare values as helper

--- src/app/components/pages/Product/CreateProduct/SelectItems.jsx
+++ src/app/components/pages/Product/CreateProduct/SelectItems.jsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useContext, useState } from 'react';
 import Container from '@material-ui/core/Container';
 import { getEndpoint } from 'helpers/api';
+import { compareValues } from 'helpers/array';
 import Datatable from './Datatable';
 import { ProductConsumer } from './ProductContext';
 
@@ -18,26 +19,6 @@ const converItemsArrayToObject = items =>
     }),
     {}
   );
-
-function compareValues(key, order = 'asc') {
-  return function innerSort(a, b) {
-    if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
-      // property doesn't exist on either object
-      return 0;
-    }
-
-    const varA = typeof a[key] === 'string' ? a[key].toUpperCase() : a[key];
-    const varB = typeof b[key] === 'string' ? b[key].toUpperCase() : b[key];
-
-    let comparison = 0;
-    if (varA > varB) {
-      comparison = 1;
-    } else if (varA < varB) {
-      comparison = -1;
-    }
-    return order === 'descending' ? comparison * -1 : comparison;
-  };
-}
 
 const SelectItems = () => {
   const { items, setItems } = useContext(ProductConsumer);
