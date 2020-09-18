690c84e6b8594c7b063a659a5662ee533539d743
roxdavirox <roxdavirox@gmail.com>
2020-09-18 04:33:31 -0300

    default component data and service encapsulate

---
+++ src/helpers/items.js
@@ -0,0 +1,19 @@
+import { convertToObjectWithKeys, addNewPropsWhen } from './array';
+import reactUuid from 'react-uuid';
+
+const defaulItemProps = {
+  price: 0,
+  quantity: 1,
+  isChecked: false
+};
+
+export const itemNotHasQuantity = item =>
+  !(item.priceTable && item.priceTable.unit === 'quantidade');
+
+export const mapDefaultItemPropsToObject = items => {
+  const itemsWithSize = addNewPropsWhen(itemNotHasQuantity)({
+    size: { x: 1, y: 1 }
+  })(items);
+  const itemsWithUuid = itemsWithSize.map(i => ({ ...i, uuid: reactUuid() }));
+  return convertToObjectWithKeys(itemsWithUuid)('uuid')(defaulItemProps);
+};
