bcbee344a1c50427b043b14fb89b5079b52d6fd0
roxdavirox <roxdavirox@gmail.com>
2020-09-18 04:36:44 -0300

    encapsulate default edit template props

--- src/app/components/pages/Option/EditTemplateItem/index.js
+++ src/app/components/pages/Option/EditTemplateItem/index.js
@@ -3,46 +3,22 @@ import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Container from '@material-ui/core/Container';
 import Datatable from './Datatable';
-import InfoItem from './InfoItem';
-import Button from '@material-ui/core/Button';
+import TemplateInfo from './TemplateInfo';
+import MoneyCard from 'app/components/common/cards/MoneyCard';
+import SaveButton from 'app/components/common/buttons/SaveButton';
+import Grid from '@material-ui/core/Grid';
+import { Breadcrumb } from 'matx';
 import { getEndpoint, createPostRequest } from 'helpers/api';
-
-const mapSizeItem = item => {
-  if (item.priceTable && item.priceTable.unit === 'quantidade') {
-    return item;
-  } else {
-    return { ...item, size: { x: 1, y: 1 } };
-  }
-};
-
-const convertIdToItemId = ({ _id: itemId, ...rest }) => ({ itemId, ...rest });
-
-const mapDefaultItemInfos = item => ({
-  ...item,
-  price: 0,
-  quantity: 1,
-  isChecked: false
-});
-
-const getTemplateItems = async () => {
-  const endpoint = getEndpoint('/items/templates');
-  const response = await fetch(endpoint);
-  const { items } = await response.json();
-  const _templateItems = items
-    .map(convertIdToItemId)
-    .map(mapSizeItem)
-    .map(mapDefaultItemInfos);
-
-  return _templateItems;
-};
+import { getTemplateItems } from './service';
 
 const TemplateItemPage = ({ location }) => {
   const { itemId } = location.state;
-  const [item, setItem] = useState({});
+  const [total, setTotal] = useState(0);
   const [options, setOptions] = useState([]);
-  const [selectedOption, setSelectOption] = useState('0');
+  const [priceTables, setPriceTables] = useState([]);
   const [templateName, setTemplateName] = useState('');
   const [templateItems, setTemplateItems] = useState([]);
+  const [templateQuantity, setTemplateQuantity] = useState(1);
 
   useEffect(() => {
     async function AsyncGetTemplateItems() {
@@ -51,9 +27,8 @@ const TemplateItemPage = ({ location }) => {
       fetch(itemsEndpoint)
         .then(res => res.json())
         .then(({ item }) => {
-          setItem(item);
           setTemplateName(item.name);
-          setSelectOption(item.option._id);
+          setTemplateQuantity(item.templateQuantity || 1);
           return item;
         })
         .then(item => {
@@ -96,10 +71,6 @@ const TemplateItemPage = ({ location }) => {
     handleCalculateTotal(rowIndex, templateItem, isChecked);
   };
 
-  const handleSelectOption = e => setSelectOption(e.target.value);
-
-  const handleChangeTemplateName = e => setTemplateName(e.target.value);
-
   const handleChangeSizeX = (rowIndex, valueX) => {
     const templateItem = {
       ...templateItems[rowIndex],
@@ -131,20 +102,6 @@ const TemplateItemPage = ({ location }) => {
   };
 
   const handleSubmit = () => {};
-
-  const calculateTotal = () => {
-    const totalTemplates = templateItems
-      .filter(tp => tp.isChecked)
-      .filter(tp => tp.itemType === 'template')
-      .reduce((acc, tp) => acc + tp.itemPrice * tp.quantity, 0);
-
-    const totalItems = templateItems
-      .filter(tp => tp.isChecked)
-      .filter(tp => tp.itemType === 'item')
-      .reduce((acc, tp) => acc + tp.itemPrice, 0);
-
-    return totalTemplates + totalItems;
-  };
 
   const handleCalculateTotal = (rowIndex, templateItem, isChecked) => {
     const { quantity, size } = templateItem;
@@ -182,43 +139,118 @@ const TemplateItemPage = ({ location }) => {
         console.log(`Error on get total value ${error}`);
       });
   };
-  const total = calculateTotal();
+
+  const handleTotalPriceCalculate = () => {
+    const checkedTemplateItems = Object.values(templateItems).filter(
+      item => item.isChecked
+    );
+
+    const checkedItems = checkedTemplateItems.filter(
+      item => item.itemType === 'item' && item.priceTable
+    );
+
+    const totalItemPrice = checkedItems
+      .filter(i => i.itemType === 'item')
+      .reduce((_total, item) => {
+        const { unitPrice = 1, unit } = priceTables[item.priceTable._id];
+        const totalPrice =
+          unit !== 'quantidade'
+            ? item.size.x * item.size.y * item.quantity * unitPrice
+            : item.quantity * unitPrice;
+        return _total + totalPrice;
+      }, 0);
+
+    const totalTemplateItemPrice = checkedTemplateItems
+      .filter(i => i.itemType === 'template' && i.priceTables)
+      .reduce((_totalTemplate, templateItem) => {
+        const totalPriceTables = Object.values(templateItem.priceTables).reduce(
+          (_totalPriceTable, pt) => {
+            const { unitPrice = 1, unit } = priceTables[pt.id];
+            const totalPrice =
+              unit !== 'quantidade'
+                ? templateItem.size.x *
+                  templateItem.size.y *
+                  templateItem.quantity *
+                  unitPrice
+                : templateItem.quantity * unitPrice;
+            const areaPriceTable = pt.area > 0 ? pt.area : 1;
+            return _totalPriceTable + totalPrice * areaPriceTable;
+          },
+          0
+        );
+        return _totalTemplate + totalPriceTables;
+      }, 0);
+
+    const _total = (totalItemPrice + totalTemplateItemPrice) * templateQuantity;
+    setTotal(_total);
+  };
 
   return (
     <>
-      <Container maxWidth="xl">
-        <p>Total: {total}</p>
+      <Container maxWidth="xl" className="m-sm-30">
+        <div className="mb-sm-30">
+          <Breadcrumb
+            routeSegments={[
+              { name: 'Opções', path: '/options' },
+              { name: 'Templates' }
+            ]}
+          />
+        </div>
+        <Container
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'flex-end',
+            maxWidth: '100%'
+          }}
+        >
+          <Grid
+            item
+            style={{
+              display: 'flex'
+            }}
+          >
+            <MoneyCard
+              title="Valor unitário"
+              value={total / templateQuantity}
+            />
+            <MoneyCard
+              title="Valor total"
+              value={total}
+              style={{ margin: '0px 16px' }}
+            />
+          </Grid>
+
+          <Grid item>
+            <SaveButton
+              initialTitle="Editar template"
+              loadingTitle="Editando template..."
+              sucessTitle="Editado com sucesso!"
+              onClick={handleSubmit}
+            />
+          </Grid>
+        </Container>
         <Container maxWidth="xl">
-          <InfoItem
-            options={options}
-            onChangeSelectOption={handleSelectOption}
-            onNameChange={handleChangeTemplateName}
-            selectedOption={selectedOption}
-            templateName={templateName}
+          <br />
+          <Datatable
+            title={
+              <TemplateInfo
+                templateName={templateName}
+                onNameChange={name => setTemplateName(name)}
+                templateQuantity={templateQuantity}
+                onChangeQuantity={quantity => setTemplateQuantity(quantity)}
+              />
+            }
+            data={templateItems}
+            onDeleteTemplateItems={handleDeleteTemplateItems}
+            onDuplicateItem={handleDuplicateItem}
+            onCheckItem={handleCheckItem}
+            onChangeSizeX={handleChangeSizeX}
+            onChangeSizeY={handleChangeSizeY}
+            onChangeQuantity={handleChangeQuantity}
+            onCalculateTotal={handleCalculateTotal}
           />
         </Container>
-        <Datatable
-          // title={}
-          data={templateItems}
-          onDeleteTemplateItems={handleDeleteTemplateItems}
-          onDuplicateItem={handleDuplicateItem}
-          onCheckItem={handleCheckItem}
-          onChangeSizeX={handleChangeSizeX}
-          onChangeSizeY={handleChangeSizeY}
-          onChangeQuantity={handleChangeQuantity}
-          onCalculateTotal={handleCalculateTotal}
-        />
-      </Container>
-      <br />
-      <Container maxWidth="xl">
-        <Button
-          onClick={handleSubmit}
-          variant="contained"
-          style={{ float: 'right' }}
-          color="primary"
-        >
-          Atualizar
-        </Button>
       </Container>
     </>
   );
